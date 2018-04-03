582e592d5cfb4c69dc139ffba08bfd52fd920464
GuilhermeAmarilho <guiamarilho1@outlook.com>
2018-04-03 15:01:23 -0300

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,16 +1,19 @@
+//adicionar ao cache todos os arquivos estáticos
 var CACHE_NAME = 'static-v1';
 self.addEventListener('install', function (event) {
-event.waitUntil(
- caches.open(CACHE_NAME).then(function (cache) {
-  return cache.addAll([
-   '/',
-   '/index.html',
-   '/styles.css',
-   '/app.js',
-   '/manifest.js',
-   '/vendor.js',
-  ]);
-})
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.js',
+                '/vendor.js',
+            ]);
+        })
+    )}
+);
 
 //Ao ativar atualiza o cache se necessário
 var CACHE_NAME = 'static-v1';
@@ -30,7 +33,6 @@ self.addEventListener('activate', function activator(event) {
 });
 
 //pegar o que for solicitado do cache, e se ele não existir fazer um
-request
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (cachedResponse) {
